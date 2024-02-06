f7f5cc99d1e7ced610dd008214f280886f961da9
camilagsoares <camila.soares19@hotmail.com>
2024-02-06 19:17:45 -0300

    .

---
+++ nlw-expert-rn/src/app/_layout.tsx
@@ -0,0 +1,9 @@
+import { SafeAreaView } from "react-native";
+import { Slot } from "expo-router";
+
+export default function Layout() {
+    return
+    <SafeAreaView className="flex-1 bg-slate-900">
+        <Slot />
+    </SafeAreaView>
+}
